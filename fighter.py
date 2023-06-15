78b9a7de477e2a071a05c0cb9e465fd4567083fa
R0N1N32 <bezdushniy666@gmail.com>
2023-06-15 06:19:13 +0600

    add attacking system

--- fighter.py
+++ fighter.py
@@ -2,11 +2,15 @@ import pygame
 
 class Fighter():
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
+        self.attacking = False
+        self.attack_type = 'NA'
+        self.health = 100
 
-    def move(self, screen_width, screen_height):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -15,15 +19,28 @@ class Fighter():
         #get keypresses
         key = pygame.key.get_pressed()
 
-        #movement
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-        #jump
-        if key[pygame.K_w] and self.jump == False:
-            self.vel_y = -30
-            self.jump = True
+        #can only perform other actions if not currently attacking
+        if self.attacking == False:
+            #movement
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            #jump
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
+            #attack
+            if key[pygame.K_j] or key[pygame.K_k]:
+                self.attack(surface, target)
+                #determine which attack type was used
+                if key[pygame.K_j]:
+                    self.attack_type = 'LA'
+                if key[pygame.K_k]:
+                    self.attack_type = 'HA'
+                if key[pygame.K_l]:
+                    self.attack_type = 'DF'
+
 
         #apply gravity
         self.vel_y += GRAVITY
@@ -39,9 +56,24 @@ class Fighter():
             self.jump = False
             dy = screen_height - 110 - self.rect.bottom
 
+        #ensure players face each other
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
+
         #update player position
         self.rect.x += dx
         self.rect.y += dy
+
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, 20)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+        #TEMP
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+        #TEMP
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
--- main.py
+++ main.py
@@ -14,6 +14,11 @@ pygame.display.set_caption('DebikFight')
 clock = pygame.time.Clock()
 FPS = 60
 
+#define colours
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 #load bacground(BG) image
 bg_image = pygame.image.load('assets/images/background/test_map.jpg').convert_alpha()
 
@@ -22,6 +27,12 @@ def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
 
+#func for drawing fighter health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 #create two instances of fighters
 fighter_1 = Fighter(200, 310)
@@ -36,8 +47,12 @@ while run:
     #draw BG
     draw_bg()
 
+    #show player stats
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
     #move fighters
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
 
     #draw fighters
     fighter_1.draw(screen)
